a3c786fcccd11f1f068c1d16ced08574d6939d42
zyadbahgat <55064732+zyadbahgat@users.noreply.github.com>
2020-08-22 16:47:25 +0200

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,24 @@
+import java.lang.Math; 
+  
+class sqrt { 
+  
+    
+    public static void main(String args[]) 
+    { 
+        double a = 30; 
+  
+        System.out.println(Math.sqrt(a)); 
+  
+        a = 45; 
+  
+        System.out.println(Math.sqrt(a)); 
+  
+        a = 60; 
+  
+        System.out.println(Math.sqrt(a)); 
+  
+        a = 90; 
+  
+        System.out.println(Math.sqrt(a)); 
+    } 
+} 
