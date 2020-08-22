0d52f6e01d0dcff15a958cfe9abe583439cdd693
ZeiadBahgat <zyadbahgat@yahoo.com>
2020-08-22 15:36:30 +0200

    added switch-case

--- calculator.java
+++ calculator.java
@@ -12,4 +12,25 @@ public class Calculator {
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
-        
+        switch (choose){
+        case 1:
+            System.out.println(add( num1,num2));
+            break;
+        case 2:
+            System.out.println(sub( num1,num2));
+            break;      
+        case 3:
+            System.out.println(mult( num1,num2));
+            break;
+        case 4:
+            System.out.println(div( num1,num2));
+            break;
+            default:
+                System.out.println("Illigal Operation");
+
+
+        }
+
+
+
+    }
